b59c1ddaefd7864d026dbce591335eef2be93119
smartr20 <smartr20@gmail.com>
2025-08-19 21:12:17 +0330

    Update models.py

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,8 +1,42 @@
 from django.db import models
 from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser, PermissionsMixin)
+from django.utils.translation import ugettext_lazy as _
 # Create your models here.
 
+class UserManager(BaseUserManager):
+    """
+        custom user manager for our App
+        
+    """
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError(_("the Email must be set"))
+        email = self.normalize_email(email)
+        is_superuser = models.BooleanField(default=False)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fileds):
+        extra_fileds.setdefault('is_staff', True)
+        extra_fileds.setdefault('is_active', True)
+        extra_fileds.setdefault('is_superuser', True)
+
+        if extra_fileds.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        
+        if extra_fileds.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        
+        return self.create_user(email, password,**extra_fileds)
+
+
 class User(AbstractBaseUser, PermissionsMixin):
+    """
+        custom user model for our App
+
+    """
     email = models.EmailField(max_length=255, unique=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
