8d741f9bd95d62854eff82e09478b1849ff12aec
smartr20 <smartr20@gmail.com>
2025-08-21 22:25:30 +0330

    r

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
 # Register your models here.
+
+
+class customUserAdmin(UserAdmin):
+    model = User
+    list_display = ('email', 'is_superuser', 'is_active')
+    list_filter = ('email', 'is_superuser', 'is_active')
+    search_fields = ('email',)
+    ordering = ('email',)
+
+
+
+admin.site.register(User, customUserAdmin)
