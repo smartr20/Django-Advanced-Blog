abe442b584e9daea4c5a28544945b7103adc8533
smartr20 <smartr20@gmail.com>
2025-08-23 21:23:44 +0330

    end of admin

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
+# getting user model object
+User = get_user_model()
 # Create your models here.
 class Post(models.Model):
     """
